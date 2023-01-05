51d126c9728c03c96d613b84c33abad49da79546
Justina <justinakliu@gmail.com>
2023-01-05 16:56:53 -0500

    updated folder structure; configured app, added Goal model

---
+++ app/__init__.py
@@ -0,0 +1,35 @@
+from flask import Flask
+from flask_sqlalchemy import SQLAlchemy
+from flask_migrate import Migrate
+import os
+from dotenv import load_dotenv
+
+
+db = SQLAlchemy()
+migrate = Migrate()
+load_dotenv()
+
+
+def create_app(test_config=None):
+    app = Flask(__name__)
+    app.config["SQLALCHEMY_TRACK_MODIFICATIONS"] = False
+
+    if test_config is None:
+        app.config["SQLALCHEMY_DATABASE_URI"] = os.environ.get(
+            "SQLALCHEMY_DATABASE_URI")
+    else:
+        app.config["TESTING"] = True
+        app.config["SQLALCHEMY_DATABASE_URI"] = os.environ.get(
+            "SQLALCHEMY_TEST_DATABASE_URI")
+
+    # Models
+    from app.models.goal import Goal
+
+    db.init_app(app)
+    migrate.init_app(app, db)
+
+    # Blueprints
+    from app.routes import goals_bp
+    app.register_blueprint(goals_bp)
+
+    return app
