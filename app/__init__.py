fa6a6a3dfe452324982c2ec72d720dc3d42cd9eb
Justina <justinakliu@gmail.com>
2023-01-10 17:17:55 -0500

    added flask_cors to requirements and configured CORS

--- app/__init__.py
+++ app/__init__.py
@@ -3,6 +3,7 @@ from flask_sqlalchemy import SQLAlchemy
 from flask_migrate import Migrate
 import os
 from dotenv import load_dotenv
+from flask_cors import CORS
 
 
 db = SQLAlchemy()
@@ -32,4 +33,5 @@ def create_app(test_config=None):
     from app.routes import goals_bp
     app.register_blueprint(goals_bp)
 
+    CORS(app)
     return app
