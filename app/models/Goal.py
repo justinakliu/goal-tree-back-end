f6482ff3092a341c6174f0ee81d48ae049af2376
Justina <justinakliu@gmail.com>
2023-01-10 14:27:20 -0500

    added cascade delete to goal model, added read_all, read_one, create_one, and delete_one routes for goals

--- app/models/Goal.py
+++ app/models/Goal.py
@@ -8,10 +8,19 @@ class Goal(db.Model):
     complete = db.Column(db.Boolean, nullable=True)
 
     parent_id = db.Column(db.Integer, db.ForeignKey("goal.id"), nullable=True)
-    children = db.relationship("Goal")
+    children = db.relationship("Goal", cascade="all, delete") # deletes children when parent are deleted
     # The relationship() configuration here works in the same way as a “normal” one-to-many relationship, with the exception that the “direction”, i.e. whether the relationship is one-to-many or many-to-one, is assumed by default to be one-to-many. 
     
-    def to_dict_tree(self):
+    def to_dict(self):
+        return {"id": self.id,
+                "title": self.title,
+                "description": self.description,
+                "complete": self.complete,
+                "parent_id": self.parent_id,
+                "children": [child.id for child in self.children]
+                }
+
+    def get_tree(self):
         tree = {"id": self.id,
                 "title": self.title,
                 "description": self.description,
@@ -20,7 +29,16 @@ class Goal(db.Model):
                 }
         
         for child in self.children:
-            tree["children"].append(child.to_dict_tree)
+            tree["children"].append(child.get_tree())
         
         return tree
-        
+
+    @classmethod
+    def from_dict(cls, data):
+        new_goal = Goal(title=data["title"],
+                    description=data["description"],
+                    parent_id = data.get("parent_id"),
+                    )
+
+        return new_goal
+
--- app/routes.py
+++ app/routes.py
@@ -2,35 +2,60 @@ from flask import Blueprint, jsonify, make_response, abort, request
 from app import db
 from app.models.goal import Goal
 
-# helper method for updating completion status
-    # update the entire tree
-    # update until done?
     
-# helper method for validating goal
+def validate_model(cls, model_id):
+    try:
+        model_id = int(model_id)
+    except:
+        abort(make_response({"message":f"{cls.__name__} {model_id} invalid"}, 400))
 
+    model = cls.query.get(model_id)
+
+    if not model:
+        abort(make_response(jsonify({"message":f"{cls.__name__} {model_id} not found"}), 404))
+    
+    return model
+
+# TO DO helper method for updating completion status
 
 goals_bp = Blueprint("goals_bp", __name__, url_prefix="/goals")
 
-# Returns all goals in database in hierarchical format
+# Returns all goals in database in array format
 @goals_bp.route("", methods=["GET"])
 def read_all_goals():
-    pass
+    goals = Goal.query.all()
+
+    goals_response = [goal.to_dict() for goal in goals]
+    return jsonify(goals_response), 200
 
 @goals_bp.route("", methods=["POST"])
 def create_goal():
-    pass
+    request_body = request.get_json() 
 
-#returns hierarchical data
-# what should the path be? /<goal_id>/tree or /<goal_id>/goals
-@goals_bp.route("/<goal_id>/goals", methods=["GET"])
+    if "title" not in request_body or "description" not in request_body:
+        return jsonify({"details": "Invalid data"}), 400
+
+    new_goal = Goal.from_dict(request_body)
+
+    db.session.add(new_goal)
+    db.session.commit()
+    
+    return jsonify({"details":"Successfully created new goal"}), 201
+
+# Returns goal tree for <goal_id> in hierarchical data
+@goals_bp.route("/<goal_id>/tree", methods=["GET"])
 def read_one_goal(goal_id):
-    pass
+    goal = validate_model(Goal, goal_id)
+    return jsonify(goal.get_tree()), 200
 
-#returns hierarchical data
-# what should the path be? /<goal_id>/tree or /<goal_id>/goals
-@goals_bp.route("/<goal_id>/goals", methods=["POST"])
-def add_goal(goal_id):
-    pass
+@goals_bp.route("/<goal_id>", methods=["DELETE"])
+def delete_goal(goal_id):
+    goal = validate_model(Goal, goal_id)
+
+    db.session.delete(goal)
+    db.session.commit()
+
+    return jsonify({"details": f'Successfully deleted'}), 200
 
 #mark goal as complete
 @goals_bp.route("/<goal_id>/complete", methods=["PATCH"])
@@ -41,8 +66,3 @@ def mark_goal_complete(goal_id, status):
 @goals_bp.route("/<goal_id>/incomplete", methods=["PATCH"])
 def mark_goal_incomplete(goal_id, status):
     pass
-
-@goals_bp.route("/<goal_id>", methods=["DELETE"])
-def delete_goal(goal_id):
-    pass
-
